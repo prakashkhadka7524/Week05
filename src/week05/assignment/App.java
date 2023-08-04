dc6351770c61a985ca74950792f647335aaa7749
prakashkhadka7524 <prakashkhadka7524@outlook.com>
2023-08-03 17:59:46 -0700

    wrote some comments

--- src/week05/assignment/App.java
+++ src/week05/assignment/App.java
@@ -2,13 +2,13 @@ package week05.assignment;
 
 public class App {
     public static void main(String[] args) {
-        AsteriskLogger asteriskLogger=new AsteriskLogger();
-        asteriskLogger.log("");
-        asteriskLogger.error("");
+        AsteriskLogger asteriskLogger=new AsteriskLogger(); // object of the AsteriskLogger class 
+        asteriskLogger.log(""); // calling method
+        asteriskLogger.error("");// calling method
 
-        SpacedLogger spacedLogger= new SpacedLogger();
-        spacedLogger.log("");
-        spacedLogger.error("");
+        SpacedLogger spacedLogger= new SpacedLogger();//  object of the SpacedLogger
+        spacedLogger.log("");// calling method
+        spacedLogger.error("");// calling method
 
 
     }
